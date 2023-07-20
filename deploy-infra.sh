ccee4603f063ad7478bb95fb3485771c4993087c
bohdan-zborovskyi-repo <bohdan.zborovskyi@gmail.com>
2023-07-20 21:08:24 +0200

    Add ALB Load Balancer

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,4 +1,4 @@
-source aws_credentials.sh
+# source aws_credentials.sh
 
 STACK_NAME=awsbootstrap
 REGION=us-east-1 
@@ -43,9 +43,9 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-    # If the deploy succeeded, show the DNS name of the created instance
+# If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value" 
 fi
