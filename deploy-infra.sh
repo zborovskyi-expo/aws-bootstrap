dac41baebdabba6066c66ca8ab70221ad3e2bdcf
bohdan-zborovskyi-repo <bohdan.zborovskyi@gmail.com>
2023-07-23 21:51:14 +0200

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -15,6 +15,10 @@ CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 echo $CODEPIPELINE_BUCKET
 
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+echo $CFN_BUCKET
+
+
 # Deploys static resources
 echo "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
@@ -24,15 +28,35 @@ aws cloudformation deploy \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo "\n\n=========== Packaging main.yml ===========" 
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:" 
+  echo $PACKAGE_ERR
+  exit 1
+fi
+
 
 # Deploy the CloudFormation template
-echo "\n\n=========== Deploying main.yml ==========="
+echo "\n\n=========== Deploying main.yml ===========" 
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file ./cfn_output/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
@@ -43,9 +67,9 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-# If the deploy succeeded, show the DNS name of the created instance
-if [ $? -eq 0 ]; then
-  aws cloudformation list-exports \
-    --profile awsbootstrap \
-    --query "Exports[?ends_with(Name,'LBEndpoint')].Value" 
+# If the deploy succeeded, show the DNS name of the endpoints
+  if [ $? -eq 0 ]; then
+    aws cloudformation list-exports \
+      --profile awsbootstrap \
+      --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
 fi
